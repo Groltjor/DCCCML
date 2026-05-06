cfcecc0f7a8ae35c9d172685ed7a3bc8ee3bd60f
Groltjor <grumnt@sclfox.com>
2026-05-06 17:31:47 -0600

    Fix on get_feature_groups PAY_0 was missing in last version

--- src/credit_default/features.py
+++ src/credit_default/features.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import pandas as pd
+import numpy as np
+
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
 
 TARGET = 'default payment next month'
 
@@ -19,7 +23,7 @@ def get_feature_groups() -> FeatureGroups:
     Define column groups used across preprocessing and modeling.
     """
 
-    payment_status_cols = [f'PAY_{i}' for i in range(1, 7)]
+    payment_status_cols = [f'PAY_{i}' for i in range(0, 7)]
     payment_status_cols.remove('PAY_1') ## No existe en el dataset
     bill_cols = [f'BILL_AMT{i}' for i in range(1, 7)]
     pay_amount_cols = [f'PAY_AMT{i}' for i in range(1, 7)]
@@ -45,6 +49,39 @@ def get_feature_groups() -> FeatureGroups:
         pay_amount_cols=pay_amount_cols,
         payment_status_cols=payment_status_cols,
     )
+
+def add_credit_behaviour_features(df: pd.DataFrame) -> pd.DataFrame:
+
+    bill_cols = [f'BILL_AMT{i}' for i in range(1, 7)]
+    pay_amount_cols = [f'PAY_AMT{i}' for i in range(1, 7)]
+
+    df = df.copy()
+
+    df["BILL_AMT_mean"] = df[bill_cols].mean(axis=1)
+    df["BILL_AMT_max"] = df[bill_cols].max(axis=1)
+    df["BILL_AMT_std"] = df[bill_cols].std(axis=1)
+
+    df["PAY_AMT_mean"] = df[pay_amount_cols].mean(axis=1)
+    df["PAY_AMT_max"] = df[pay_amount_cols].max(axis=1)
+    df["PAY_AMT_std"] = df[pay_amount_cols].std(axis=1)
+
+    df['debt_to_limit'] = np.where(
+        df['LIMIT_BAL'] > 0,
+        df['BILL_AMT_mean'] / df['LIMIT_BAL'],
+        0
+    )
+
+    df['payment_to_debt'] = np.where(
+        df['BILL_AMT_mean'] > 0,
+        df['PAY_AMT_mean'] / df['BILL_AMT_mean'],
+        0
+    )
+
+    columnas_anadidas = ['BILL_AMT_mean', 'BILL_AMT_max', 'BILL_AMT_std', 
+                          'PAY_AMT_mean', 'PAY_AMT_max', 'PAY_AMT_std', 'debt_to_limit', 
+                          'payment_to_debt']
+
+    return df, columnas_anadidas
 
 def clean_credit_data(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -76,3 +113,31 @@ def split_features_target(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
     y = df[TARGET]
 
     return X, y
+
+def check_for_vif(df: pd.DataFrame, numeric_cols: list[str]):
+    """ 
+    """
+
+    vif_df = df[numeric_cols].copy()
+
+    X = vif_df.copy()
+    X_const = add_constant(X)
+
+    vif_data = pd.DataFrame()
+    vif_data['variable']  = X_const.columns
+    vif_data['VIF'] = [
+        variance_inflation_factor(X_const.values, i)
+        for i in range(X_const.shape[1])
+    ]
+
+    vif_data = (
+        vif_data[vif_data['variable'] != 'constant']
+        .sort_values('VIF', ascending = False)
+    )
+
+    return vif_data
+
+
+
+    
+
