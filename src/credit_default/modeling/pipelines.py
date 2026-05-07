899046c18a0ee4c987f7d35a4191a7494eab9b51
Groltjor <grumnt@sclfox.com>
2026-05-06 18:01:39 -0600

    Updates en pipelines. Agregado el pipeline log1p que ahora integra a las columnas con valores no negativos la transformacion, problema, sigue siendo manual, el modelo mejoro respecto a no usar log1p. Tambien toque 02 FE notebok

--- src/credit_default/modeling/pipelines.py
+++ src/credit_default/modeling/pipelines.py
@@ -7,6 +7,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 
+from sklearn.preprocessing import FunctionTransformer
+
+import numpy as np
+
 from xgboost import XGBClassifier
 
 
@@ -37,6 +41,38 @@ def build_preprocessor(
 
     return preprocessor
 
+def build_preprocessor_np1log(
+    pos_num_cols: list[str],
+    neg_num_cols: list[str],
+    categorical_cols: list[str],
+) -> ColumnTransformer:
+
+    log1p_transformer = Pipeline(steps=[
+        ('log1p', FunctionTransformer(np.log1p, feature_names_out='one-to-one')),
+        ('scaler', StandardScaler())
+    ])
+
+    numeric_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler()),
+    ])
+
+    categorical_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore")),
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('log1p_num', log1p_transformer, pos_num_cols),
+            ('num', numeric_pipeline, neg_num_cols),
+            ('cat', categorical_pipeline, categorical_cols),
+        ],
+        remainder="drop",
+    )
+
+    return preprocessor
+
 
 def build_logistic_regression_pipeline(
     numeric_cols: list[str],
@@ -59,6 +95,30 @@ def build_logistic_regression_pipeline(
     pipeline = Pipeline(steps=[
         ("preprocessor", preprocessor),
         ("model", model),
+    ])
+
+    return pipeline
+
+def build_logistic_regression_pipeline_np1log(
+    pos_num_cols: list[str],
+    neg_num_cols: list[str],
+    categorical_cols: list[str],
+) -> Pipeline:
+    preprocessor = build_preprocessor_np1log(
+        pos_num_cols = pos_num_cols,
+        neg_num_cols = neg_num_cols,
+        categorical_cols = categorical_cols,
+    )
+
+    model = LogisticRegression(
+        max_iter = 1000,
+        class_weight="balanced",
+        random_state= 42
+    )
+
+    pipeline = Pipeline(steps =[
+        ('preprocessor', preprocessor),
+        ('model', model)
     ])
 
     return pipeline
